e48f0fc73769b40a55e6f357a78703121a4a975e
Ken <ken@iMac-2014-Ken.local>
2020-03-02 11:14:35 -0500

    add pwa plugin

--- some-theme/inc/plugins.php
+++ some-theme/inc/plugins.php
@@ -16,12 +16,18 @@ add_action( 'tgmpa_register', function() {
 				'is_callable'      => 'acf',
 				'force_activation' => true,
 			],
+			[
+				'name'             => 'PWA',
+				'slug'             => 'pwa',
+				'required'         => false,
+				'force_activation' => false,
+			],
 		],
 		[
 			'id'           => 'sometheme',             // Unique ID for hashing notices for multiple instances of TGMPA.
 			'menu'         => 'tgmpa-install-plugins', // Menu slug.
 			'has_notices'  => true,                    // Show admin notices or not.
-			'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
+			'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
 			'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
 			'is_automatic' => false,                   // Automatically activate plugins after installation or not.
 			'message'      => '',                      // Message to output right before the plugins table.
