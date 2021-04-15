7cd77cf1fac74ffd0610d19f86ef74768221eea0
Ken Newman <wraithkenny@unfocus.com>
2021-04-15 13:47:52 -0400

    updates

--- dev/bin/docker-ready.sh
+++ dev/bin/docker-ready.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Launch the WordPress docker
-docker-compose -f dev/docker-compose.yml up -d
+docker-compose -f dev/docker-compose.yml --project-directory . up -d
 
 # Wait until the docker containers are setup properely
 echo -n "Waiting for server..."
