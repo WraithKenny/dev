48e15ef76787177187930aba87ba2911ac78a61d
Ken Newman <ken@ashworthcreative.com>
2022-01-21 13:24:41 -0500

    update defer css technique

--- some-theme/inc/scripts-styles.php
+++ some-theme/inc/scripts-styles.php
@@ -101,10 +101,11 @@ add_filter( 'script_loader_tag', function( $tag, $handle ) {
 /**
  * "Defer" CSS using `wp_style_add_data( 'handle', 'async', true );`.
  * See: https://web.dev/defer-non-critical-css/#optimize
+ * See: https://www.filamentgroup.com/lab/load-css-simpler/
  */
 add_filter( 'style_loader_tag', function( $tag, $handle, $href, $media ) {
 	if ( wp_styles()->get_data( $handle, 'async' ) ) {
-		$tag = '<link rel="preload" id="' . $handle . '-css" media="' . $media . '" href="' . $href . '" as="style" onload="this.onload=null;this.rel=\'stylesheet\'">
+		$tag = '<link rel="stylesheet" id="' . $handle . '-css" media="print" href="' . $href . '" onload="this.media=\'' . $media . '\';this.onload=null;">
 		<noscript>' . $tag . '</noscript>';
 	}
 	return $tag;
@@ -123,16 +124,6 @@ add_filter( 'body_class', function( $classes ) {
 	$classes[] = 'no-js';
 	return $classes;
 } );
-
-/**
- * Include minified loadCSS. (Polyfil for Firefox.)
- * https://github.com/filamentgroup/loadCSS/blob/v2.1.0/src/cssrelpreload.js
- */
-add_action( 'wp_head', function() {
-	?>
-	<script>/*! loadCSS. [c]2017 Filament Group, Inc. MIT License */!function(t){"use strict";t.loadCSS||(t.loadCSS=function(){});var e=loadCSS.relpreload={};if(e.support=function(){var e;try{e=t.document.createElement("link").relList.supports("preload")}catch(t){e=!1}return function(){return e}}(),e.bindMediaToggle=function(t){var e=t.media||"all";function a(){t.addEventListener?t.removeEventListener("load",a):t.attachEvent&&t.detachEvent("onload",a),t.setAttribute("onload",null),t.media=e}t.addEventListener?t.addEventListener("load",a):t.attachEvent&&t.attachEvent("onload",a),setTimeout(function(){t.rel="stylesheet",t.media="only x"}),setTimeout(a,3e3)},e.poly=function(){if(!e.support())for(var a=t.document.getElementsByTagName("link"),n=0;n<a.length;n++){var o=a[n];"preload"!==o.rel||"style"!==o.getAttribute("as")||o.getAttribute("data-loadcss")||(o.setAttribute("data-loadcss",!0),e.bindMediaToggle(o))}},!e.support()){e.poly();var a=t.setInterval(e.poly,500);t.addEventListener?t.addEventListener("load",function(){e.poly(),t.clearInterval(a)}):t.attachEvent&&t.attachEvent("onload",function(){e.poly(),t.clearInterval(a)})}"undefined"!=typeof exports?exports.loadCSS=loadCSS:t.loadCSS=loadCSS}("undefined"!=typeof global?global:this);</script>
-	<?php
-}, 11 );
 
 /**
  * Support for `hamburgers` JavaScript portion.
