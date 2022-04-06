47e2ed8be118b7270ef65fdb38f254f098734ffd
Ken Newman <ken@ashworthcreative.com>
2022-04-06 11:37:17 -0400

    clean up admin-bump

--- some-theme/functions.php
+++ some-theme/functions.php
@@ -15,21 +15,11 @@
  * The result is an admin bar that doesn't take space but moves
  * itself out of the way visually.
  */
-// phpcs:disable
-add_theme_support( 'admin-bar', [ 'callback' => function() {
-	?><style id="admin-bar-style">
-	#wpadminbar {
-		opacity: 0.25;
-		transform: translateY(-50%);
-		transition: opacity 0.3s, transform 0.3s;
-	}
-	#wpadminbar:hover {
-		opacity: 1;
-		transform: translateY(0%);
-	}
-	</style><?php
-} ] );
-// phpcs:enable
+add_theme_support( 'admin-bar', [
+	'callback' => function() {
+		echo '<style id="admin-bar-style">#wpadminbar {opacity:0.25;transform:translateY(-50%);transition:opacity 0.3s, transform 0.3s;}#wpadminbar:hover {opacity:1;transform:translateY(0%);}</style>';
+	},
+]);
 
 require_once get_theme_file_path( 'inc/update-check.php' );
 require_once get_theme_file_path( 'inc/acf.php' );
